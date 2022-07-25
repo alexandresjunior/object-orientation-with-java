ba3fe054ba3b7b4885c570c92d3672de174fbbdd
Alexandre de Souza Jr <83607914+alexandresjunior@users.noreply.github.com>
2022-07-25 16:03:53 -0300

    Create byte bank app project

---
+++ bytebank/src/main/java/com/bytebank/app/ByteBankApp.java
@@ -0,0 +1,9 @@
+package com.bytebank.app;
+
+public class ByteBankApp {
+    
+    public static void main(String[] args) {
+        System.out.println("Hello, Byte Bank World!");
+    }
+
+}
---
+++ bytebank/src/test/java/com/bytebank/app/ByteBankAppTest.java
@@ -0,0 +1,14 @@
+package com.bytebank.app;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class ByteBankAppTest {
+
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue( true );
+    }
+    
+}
